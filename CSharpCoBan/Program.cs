79f37631ad829dc983e1e7808a9c5b1ac056dc37
quan91 <quan91.nt@gmail.com>
2024-03-10 22:42:21 +0700

    Buoi 1

---
+++ CSharpCoBan/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpCoBan
+{
+    public class Program
+    {
+
+        public int Tong(int a, int b)
+        {
+            return a + b;
+        }
+        static void Main(string[] args)
+        {
+            string myName = "My name is Quan";
+
+
+
+            const int MyInt = 100000;
+            long _myLong = 1000000;
+
+            // Đặt tên biến làm sao cho tường mình và mang tính chất gợi nhớ 
+            // cameCase 
+            // Pascal Case 
+            // underScore _
+
+            var myVarVariable = 100;
+            var myVarVariable2 = "string";
+
+            if (MyInt > 10 && MyInt < 1000001)
+            {
+                Console.WriteLine("myName : " + myName);
+            }
+            var sum = MyInt * myVarVariable;
+
+            Console.WriteLine("myName : " + myName);
+            Console.WriteLine("sum : " + sum);
+          
+
+            int inPut = 0;
+            Console.WriteLine("Mời nhập số :");
+            inPut = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("số ban vừa nhập là : " + inPut);
+
+            Console.ReadKey();
+        }
+
+
+
+    }
+}
---
+++ WebNetFrameWork/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("WebNetFrameWork")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("WebNetFrameWork")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("6a83450a-f42f-4f95-b380-2f6fd6631018")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
