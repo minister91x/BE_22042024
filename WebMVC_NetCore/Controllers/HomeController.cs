f776387ae3b86395f50800af44c3901604c75702
quan91 <quan91.nt@gmail.com>
2024-05-19 22:36:12 +0700

    buoi16

---
+++ WebMVC_NetCore/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using WebMVC_NetCore.Models;
+
+namespace WebMVC_NetCore.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            var model = new List<BookModels>();
+
+            // đưa dữ liệu vào model
+            for (int i = 0; i < 5; i++)
+            {
+                model.Add(new BookModels
+                {
+                    BookID = i + 1,
+                    BookName = "Sách số " + (i + 1)
+                });
+            }
+
+            // Trả dữ liệu thông qua model về View
+            return View(model);
+          
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ WebMVC_NetCore/Models/BookModels.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebMVC_NetCore.Models
+{
+    public class BookModels
+    {
+        public int BookID { get; set; }
+        public string BookName { get; set; }
+    }
+
+    public class BookModels1
+    {
+        public int BookID { get; set; }
+        public string BookName { get; set; }
+    }
+}
